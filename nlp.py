9f4378c4d38d5daafd4bdfa9f2a8ab236c95750e
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2021-11-11 23:31:19 +0000

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -1,6 +1,5 @@
 import spacy_streamlit
 import streamlit as st
 models = ['en_core_web_trf']
-nlp = spacy.load('en_core_web_trf')
 texts = st.text_area('Please type your texts here.')
 spacy_streamlit.visualize(models, texts)
